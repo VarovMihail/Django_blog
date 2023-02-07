3ae5603fbdf9fa0512c09b21ae3fedbb3815841f
mihail <8605495@mail.ru>
2023-02-07 20:08:34 +0300

    change prod.yml

--- web/api/v1/action/swagger_schemas.py
+++ web/api/v1/action/swagger_schemas.py
@@ -1,9 +1,36 @@
+from rest_framework import serializers
+
+
+class ResponseLikeDislikeSerializer(serializers.Serializer):
+    user = serializers.IntegerField()
+    vote = serializers.IntegerField()
+    model = serializers.CharField()
+    object_id = serializers.IntegerField()
+
+
+class SubscribeSerializer(serializers.Serializer):
+    subscribe_status = serializers.IntegerField()
+    followers_count = serializers.IntegerField()
+    following_count = serializers.IntegerField()
 
 
 like_dislike_schema = {
-    'operation_description': '- hello',
-    'operation_summary': 'set like or dislike',
+    'operation_description': '## Set like to article/comment',
+    'operation_summary': 'set like or dislike to articles and comments',
+    'tags': ['Likes'],
+    'operation_id': 'get_Likes',
+    'responses': {200: ResponseLikeDislikeSerializer},
 }
+
+
+subscribe_schema = {
+    'operation_description': '## Subscribe / Unsubscribe to User',
+    'operation_summary': 'add / delete user to following',
+    'tags': ['Subscribes'],
+    'operation_id': 'get_Subscribe',
+    'responses': {200: SubscribeSerializer},
+}
+
 
 
 
